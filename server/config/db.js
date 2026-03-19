eef9dbae51eaf38477100793df695f3a4fcd2c91
Sanjanaanil <sanjananaik335@gmail.com>
2026-03-20 01:34:17 +0530

    fix db.js merge conflict

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
@@ -14,21 +14,4 @@ const connectDB = async () => {
   }
 };
 
-=======
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected Successfully');
-  } catch (error) {
-    console.error('MongoDB Connection Error:', error);
-    process.exit(1);
-  }
-};
-
->>>>>>> de7565ca49d3668c0675e7bf4d5a8e6c7249fdb9
 module.exports = connectDB;
